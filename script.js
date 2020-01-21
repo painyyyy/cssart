7cd8b6c742d5182e79ba3a6dd9b7faf5da73fe3d
Pâiny <elviraionita@gmail.com>
2020-01-21 16:41:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+ var h = $('#head');
+   function runIt() {
+       h.animate({top:'+=20'}, 1000);
+       h.animate({top:'-=20'}, 1000, runIt);
+   }
+
+   runIt();
+   
+  var f = $('#floating');
+   function rrunIt() {
+      h.animate({top:'+=20'}, 1000);
+      h.animate({top:'-=20'}, 1000, runIt);
+      f.animate({width:'+=10', left:'-=5'}, 1000);
+      f.animate({width:'-=10', left:'+=5'}, 1000, rrunIt);
+     
+       
+   }
+
+   rrunIt();
+
+   // taken from here: https://stackoverflow.com/questions/12738558/how-to-make-a-div-move-up-and-down-like-a-floating-baloon
